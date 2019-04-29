d5e638f2036972826cd33aaaec50424df24d2a9e
Irvin Noe Valeriano Cortes <irvin.valeriano@bi-dss.com>
2019-04-29 16:39:15 -0600

    CAMBIOS CAI

--- ERPMVC/Controllers/CAIController.cs
+++ ERPMVC/Controllers/CAIController.cs
@@ -1,16 +1,130 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
+using ERPMVC.Helpers;
+using ERPMVC.Models;
+using Kendo.Mvc.Extensions;
+using Kendo.Mvc.UI;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using Newtonsoft.Json;
 
 namespace ERPMVC.Controllers
 {
+    [Authorize]
+    [CustomAuthorization]
     public class CAIController : Controller
     {
-        public IActionResult Index()
+        private readonly IOptions<MyConfig> config;
+        //  private readonly ApplicationDbContext _context;
+
+        public CAIController(IOptions<MyConfig> config)
+        {
+            this.config = config;
+            //this._context = context;
+        }
+
+        // GET: Customer
+        public ActionResult Index()
         {
             return View();
         }
-    }
+
+        // POST: CAI/Create
+        [HttpPost]
+        //[ValidateAntiForgeryToken]
+        public async Task<ActionResult> Post(CAI _CAI)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+                string baseadress = config.Value.urlbase;
+                HttpClient _client = new HttpClient();
+                _client.DefaultRequestHeaders.Add("Authorization", "Bearer " + HttpContext.Session.GetString("token"));
+                _CAI.UsuarioCreacion = HttpContext.Session.GetString("user");
+                _CAI.UsuarioModificacion = HttpContext.Session.GetString("user");
+                var result = await _client.PostAsJsonAsync(baseadress + "api/CAI/Insert", _CAI);
+                string valorrespuesta = "";
+                if (result.IsSuccessStatusCode)
+                {
+                    valorrespuesta = await (result.Content.ReadAsStringAsync());
+                    _CAI = JsonConvert.DeserializeObject<CAI>(valorrespuesta);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                return BadRequest($"Ocurrio un error{ex.Message}");
+            }
+
+            return new ObjectResult(new DataSourceResult { Data = new[] { _CAI }, Total = 1 });
+        }
+                     
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Put(Int64 id, CAI _CAI)
+        {
+            try
+            {
+                string baseadress = config.Value.urlbase;
+                HttpClient _client = new HttpClient();
+                _client.DefaultRequestHeaders.Add("Authorization", "Bearer " + HttpContext.Session.GetString("token"));
+
+                var result = await _client.PutAsJsonAsync(baseadress + "api/CAI/Update", _CAI);
+                string valorrespuesta = "";
+                if (result.IsSuccessStatusCode)
+                {
+                    valorrespuesta = await (result.Content.ReadAsStringAsync());
+                    _customer = JsonConvert.DeserializeObject<Customer>(valorrespuesta);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                return BadRequest($"Ocurrio un error{ex.Message}");
+            }
+
+            return new ObjectResult(new DataSourceResult { Data = new[] { _CAI }, Total = 1 });
+        }
+
+        [HttpDelete("[action]")]
+        public async Task<ActionResult<ApplicationRole>> DeleteCAI(ApplicationRole _CAI)
+        {
+            try
+            {
+                string baseadress = config.Value.urlbase;
+                HttpClient _client = new HttpClient();
+
+                _client.DefaultRequestHeaders.Add("Authorization", "Bearer " + HttpContext.Session.GetString("token"));
+                var result = await _client.PostAsJsonAsync(baseadress + "api/Roles/Delete", _CAI);
+                string valorrespuesta = "";
+                if (result.IsSuccessStatusCode)
+                {
+                    valorrespuesta = await (result.Content.ReadAsStringAsync());
+                    _rol = JsonConvert.DeserializeObject<ApplicationRole>(valorrespuesta);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                return BadRequest($"Ocurrio un error{ex.Message}");
+            }
+
+            return new ObjectResult(new DataSourceResult { Data = new[] { _CAI }, Total = 1 });
+        }
+
+
+
+
+
+
+ ///----------------------------------------------------------------------------------------
+    //    public IActionResult Index()
+    //    {
+    //        return View();
+    //    }
+    //}
 }
