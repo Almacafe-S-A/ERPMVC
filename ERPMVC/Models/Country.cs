09a87f2eea90a278b2ea57a97d8c2430692e59f2
Cristopher Arias <Ariasover@users.noreply.github.com>
2019-06-25 10:55:16 -0600

    Mantenimientos pendientes
    Lista de mantenimientos que estaban pendientes, en este caso Country
    y pruebas hechas en  Account

---
+++ ERPMVC/Models/Country.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ERPAPI.Models
+{
+    public class Country
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public long Id { get; set; } // bigint
+        public string SortName { get; set; } // text
+        public string Name { get; set; } // text
+        public int? PhoneCode { get; set; } // integer
+
+        public string Usuariocreacion { get; set; }
+        public string Usuariomodificacion { get; set; }
+        public DateTime? FechaCreacion { get; set; }
+        public DateTime? FechaModificacion { get; set; }
+    }
+
+
+}
