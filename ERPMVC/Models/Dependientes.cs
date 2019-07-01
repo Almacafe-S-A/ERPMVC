b6b77690ec2e9d02d607454d2a1d1f94e98fd767
Cristopher Arias <Ariasover@users.noreply.github.com>
2019-07-01 08:45:47 -0600

    Ignorar el appsettings.json
    ignorar el archivo y avance en mantenimientos a las tablas

---
+++ ERPMVC/Models/Dependientes.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace ERPMVC.Models
+{
+    public class Dependientes
+    {
+
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public long IdDependientes { get; set; }
+        public string NombreDependientes { get; set; }
+        public string Parentezco { get; set; }
+        public string Telefono { get; set; }
+        public string Direccion { get; set; }
+        public long? IdEmpleado { get; set; }
+
+        public string Usuariocreacion { get; set; }
+        public string Usuariomodificacion { get; set; }
+        public DateTime? FechaCreacion { get; set; }
+        public DateTime? FechaModificacion { get; set; }
+    }
+}
+
