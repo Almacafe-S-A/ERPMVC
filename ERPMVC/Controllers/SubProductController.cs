bec33c0da51c1b733d759dee8cee0c67cdef1e22
Freddy Chinchilla <fchinchillahn@gmail.com>
2019-07-14 23:42:36 -0600

    ReciboMercaderia_Kardex

--- ERPMVC/Controllers/SubProductController.cs
+++ ERPMVC/Controllers/SubProductController.cs
@@ -177,7 +177,7 @@ namespace ERPMVC.Controllers
                 string baseadress = config.Value.urlbase;
                 HttpClient _client = new HttpClient();
                 _client.DefaultRequestHeaders.Add("Authorization", "Bearer " + HttpContext.Session.GetString("token"));
-                var result = await _client.GetAsync(baseadress + "api/SubProduct/GetSubProductbById/" + SubProductId);
+                var result = await _client.GetAsync(baseadress + "api/SubProduct/GetSubProductById/" + SubProductId);
                 string valorrespuesta = "";
                 if (result.IsSuccessStatusCode)
                 {
