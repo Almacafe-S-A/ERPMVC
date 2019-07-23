f4b2592d3fa91e77b50b55b90e16d8662b5e69ba
Cristopher Arias <Ariasover@users.noreply.github.com>
2019-07-23 14:09:12 -0600

    Fixed bug Empresa

---
+++ ERPMVC/Models/Empresa.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ERPMVC.Models
+{
+    public class Empresa
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public long IdEmpresa { get; set; }
+        public string NombreEmpresa { get; set; }
+        public string NombreContacto { get; set; }
+        public string Telefono { get; set; }
+        public string Direccion { get; set; }
+
+        public string Usuariocreacion { get; set; }
+        public string Usuariomodificacion { get; set; }
+        public DateTime? FechaCreacion { get; set; }
+        public DateTime? FechaModificacion { get; set; }
+    }
+}
