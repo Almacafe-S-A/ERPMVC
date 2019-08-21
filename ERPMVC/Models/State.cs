bc18cf9fb2340aaae6e5a709df48a1bb8441db1a
Cristopher Arias <ariasover@gmail.com>
2019-08-21 15:28:08 -0600

    Mantenimientos
    State
    City fixed
    Sidebar

--- ERPMVC/Models/State.cs
+++ ERPMVC/Models/State.cs
@@ -12,19 +12,22 @@ namespace ERPMVC.Models
         [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         [Display(Name = "Id")]
         public long Id { get; set; } // bigint
-        [Display(Name = "Nombre")]
+        [Display(Name = "Departamento")]
         public string Name { get; set; } // text
-        [Display(Name = "Id País")]
-        public long? Country_id { get; set; } // bigint        
+
+
         [Display(Name = "Usuario de creación")]
         public string Usuariocreacion { get; set; }
+
         [Display(Name = "Usuario de modificación")]
         public string Usuariomodificacion { get; set; }
+
         [Display(Name = "Fecha de creación")]
         public DateTime? FechaCreacion { get; set; }
-
         [Display(Name = "Fecha de modificación")]
         public DateTime? FechaModificacion { get; set; }
+
+        public Int64 CountryId { get; set; }
 
         public List<City> City { get; set; }
     }
