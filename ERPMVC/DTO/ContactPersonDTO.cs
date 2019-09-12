9e2b90768b53270e83d198a2cfe2b6a357653406
Marvin Guillen <marvin.a.guillen@gmail.com>
2019-09-11 18:16:02 -0600

    Changes of Proveedores, ContactPersonDTO, ContactPerson.cshtml

---
+++ ERPMVC/DTO/ContactPersonDTO.cs
@@ -0,0 +1,18 @@
+﻿using ERPMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ERPMVC.DTO
+{
+    public class ContactPersonDTO : ContactPerson
+    {
+        public List<ContactPerson> _ContactPerson { get; set; }
+
+        public int editar { get; set; } = 1;
+
+        public string token { get; set; }
+
+    }
+}
