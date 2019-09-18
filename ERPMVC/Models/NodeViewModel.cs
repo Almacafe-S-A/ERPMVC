2a93ed3d9adc2a11bc796cf6c101bbaf2e4bdf01
Marvin Guillen <marvin.a.guillen@gmail.com>
2019-09-17 23:31:25 -0600

    Changes of Accounting Controller, Configuration Vendor View, Model and Controller

---
+++ ERPMVC/Models/NodeViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ERPMVC.Models
+{
+    public class NodeViewModel
+    {
+        public NodeViewModel()
+        {
+            this.Expanded = true;
+            this.Children = new List<NodeViewModel>();
+        }
+
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public bool Expanded { get; set; }
+
+        public bool HasChildren
+        {
+            get { return Children.Any(); }
+        }
+
+        public IList<NodeViewModel> Children { get; private set; }
+
+    }
+}
