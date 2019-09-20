8e767b64df2dbcd762926e2b167c4ba942fdf44a
Osdy <osdi@hotmail.es>
2019-09-20 12:14:58 -0600

    .

---
+++ ERPMVC/DTO/EmployeeDocumentDTO.cs
@@ -0,0 +1,15 @@
+﻿using ERPMVC.Models;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ERPMVC.DTO
+{
+    public class EmployeeDocumentDTO : EmployeeDocument
+    {
+        public IEnumerable<IFormFile> files { get; set; }
+
+    }
+}
