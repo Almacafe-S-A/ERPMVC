f50d883eb50bb248e3d0ce50004ea64df3d0d6c5
anjimenez <44504837+anjimenez@users.noreply.github.com>
2019-09-20 22:51:24 -0600

    Cambios en modelo JournelEntry, vista pvwWarehouse, vista Warehouse, vista pvwAddJournalEntry

--- ERPMVC/Models/JournalEntry.cs
+++ ERPMVC/Models/JournalEntry.cs
@@ -10,8 +10,9 @@ namespace ERPMVC.Models
     public class JournalEntry
     {
         [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Display(Name = "Id Asiento Contable")]
         public Int64 JournalEntryId { get; set; }
-
+       
         public JournalEntry()
         {
             JournalEntryLines = new HashSet<JournalEntryLine>();
@@ -19,23 +20,28 @@ namespace ERPMVC.Models
 
         [Display(Name = "Id Libro Mayor")]
         public int? GeneralLedgerHeaderId { get; set; }
+        [Display(Name = "Socio de negocio")]
         public int? PartyId { get; set; }
         [Display(Name = "Tipos de Voucher")]
         public Int32 VoucherType { get; set; }
         [Display(Name = "Nombre de Tipo de Voucher")]
         public string TypeJournalName { get; set; }
-
-        [Display(Name = "Fecha de creacion")]
+        [Display(Name = "Fecha de creación")]
         public DateTime Date { get; set; }
         [Display(Name = "Fecha de Posteo")]
         public DateTime DatePosted { get; set; }
+        [Display(Name = "Memo")]
         public string Memo { get; set; }
+        [Display(Name = "Número de referencia")]
         public string ReferenceNo { get; set; }
+        [Display(Name = "Estado")]
         public bool? Posted { get; set; }
+        [Display(Name = "Libro contable")]
         public virtual GeneralLedgerHeader GeneralLedgerHeader { get; set; }
       //  public virtual int Party { get; set; }
         [Display(Name = "Id de Registro Pago")]
         public Int32 IdPaymentCode { get; set; }
+        [Display(Name = "Tipo de Pago")]
         public Int32 IdTypeofPayment { get; set; }
         public virtual ICollection<JournalEntryLine> JournalEntryLines { get; set; }
         [Required]
