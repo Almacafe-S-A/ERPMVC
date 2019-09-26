7981cb066509779f2392d65002260499da1861dc
Freddy Chinchilla <fchinchillahn@gmail.com>
2019-09-26 14:03:57 -0600

    TreeViewAccounting

---
+++ ERPMVC/DTO/AccountingDTO.cs
@@ -0,0 +1,14 @@
+﻿using ERPMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ERPMVC.DTO
+{
+    public class AccountingDTO : Accounting
+    {
+
+        public List<AccountingDTO> Children = new List<AccountingDTO>();
+    }
+}
