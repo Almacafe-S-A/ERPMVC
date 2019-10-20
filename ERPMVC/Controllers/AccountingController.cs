79f6a1c2eed7d529b6c8df9a753489eb25762e93
Marvin Guillen <marvin.a.guillen@gmail.com>
2019-10-20 00:03:44 -0600

    Change of InsurancesCertificate

--- ERPMVC/Controllers/AccountingController.cs
+++ ERPMVC/Controllers/AccountingController.cs
@@ -264,7 +264,7 @@ namespace ERPMVC.Controllers
         }
         async Task<IEnumerable<TypeAccount>> ObtenerTypeAccount()
         {
-            IEnumerable<TypeAccount> branches = null;
+            IEnumerable<TypeAccount> typesAccount = null;
             try
             {
                 string baseadress = config.Value.urlbase;
@@ -276,7 +276,7 @@ namespace ERPMVC.Controllers
                 if (result.IsSuccessStatusCode)
                 {
                     valorrespuesta = await (result.Content.ReadAsStringAsync());
-                    branches = JsonConvert.DeserializeObject<IEnumerable<TypeAccount>>(valorrespuesta);
+                    typesAccount = JsonConvert.DeserializeObject<IEnumerable<TypeAccount>>(valorrespuesta);
 
                 }
             }
@@ -285,8 +285,8 @@ namespace ERPMVC.Controllers
                 _logger.LogError($"Ocurrio un error: { ex.ToString() }");
                 throw ex;
             }
-            ViewData["defaultAccount"] = branches.FirstOrDefault();
-            return branches;
+            ViewData["defaultAccount"] = typesAccount.FirstOrDefault();
+            return typesAccount;
 
         }
         /*
@@ -366,7 +366,7 @@ namespace ERPMVC.Controllers
         }
 
 
-        
+        //[Authorize(Policy = "Admin")]
         public async Task<ActionResult> pvwTreeAccounting(Int64 TypeAccountId)
         {
             try
