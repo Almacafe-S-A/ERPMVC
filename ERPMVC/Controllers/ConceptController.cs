285844c23e10bc154f8b6d23181da4a7a5425327
anjimenez <44504837+anjimenez@users.noreply.github.com>
2019-10-24 14:33:58 -0600

    ConceptController, ConceptDTO, Concept Models, Views, JournalEntryLine

---
+++ ERPMVC/Controllers/ConceptController.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using ERPMVC.Helpers;
+using ERPMVC.Models;
+using ERPMVC.DTO;
+using Kendo.Mvc.Extensions;
+using Kendo.Mvc.UI;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Newtonsoft.Json;
+
+namespace ERPMVC.Controllers
+{
+    [Authorize]
+    [CustomAuthorization]
+    [ResponseCache(NoStore = true, Location = ResponseCacheLocation.None)]
+    public class ConceptController : Controller
+    {
+        private readonly IOptions<MyConfig> config;
+        private readonly ILogger _logger;
+        public ConceptController(ILogger<BankController> logger, IOptions<MyConfig> config)
+        {
+            this.config = config;
+            this._logger = logger;
+        }
+
+        public IActionResult Concept()
+        {
+            return View();
+        }
+
+
+        public async Task<ActionResult> pvwAddConcept([FromBody]ConceptDTO _sarpara)
+        {
+            ConceptDTO _Concept = new ConceptDTO();
+            try
+            {
+                string baseadress = config.Value.urlbase;
+                HttpClient _client = new HttpClient();
+                _client.DefaultRequestHeaders.Add("Authorization", "Bearer " + HttpContext.Session.GetString("token"));
+                var result = await _client.GetAsync(baseadress + "api/Concept/GetConceptById/" + _sarpara.ConceptId);
+                string valorrespuesta = "";
+                if (result.IsSuccessStatusCode)
+                {
+                    valorrespuesta = await (result.Content.ReadAsStringAsync());
+                    _Concept = JsonConvert.DeserializeObject<ConceptDTO>(valorrespuesta);
+                }
+                if (_Concept == null)
+                {
+                    _Concept = new ConceptDTO();
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Ocurrio un error: { ex.ToString() }");
+                throw ex;
+            }
+            return PartialView(_Concept);
+        }
+
+
+
+        [HttpGet("[action]")]
+        public async Task<DataSourceResult> GetConcept([DataSourceRequest]DataSourceRequest request)
+        {
+            List<Concept> _Concept = new List<Concept>();
+            try
+            {
+                string baseadress = config.Value.urlbase;
+                HttpClient _client = new HttpClient();
+                _client.DefaultRequestHeaders.Add("Authorization", "Bearer " + HttpContext.Session.GetString("token"));
+                var result = await _client.GetAsync(baseadress + "api/Concept/GetConcept");
+                string valorrespuesta = "";
+                if (result.IsSuccessStatusCode)
+                {
+                    valorrespuesta = await (result.Content.ReadAsStringAsync());
+                    _Concept = JsonConvert.DeserializeObject<List<Concept>>(valorrespuesta);
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Ocurrio un error: { ex.ToString() }");
+                throw ex;
+            }
+            return _Concept.ToDataSourceResult(request);
+        }
+
+
+    }
+}
