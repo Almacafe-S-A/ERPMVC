690f2a8ebf9514c415916900bf0214acdae1675f
Maria Alejandra Funez <44504798+Alejandrafp@users.noreply.github.com>
2019-10-31 17:13:09 -0600

    InsurancesCertificate Modificaciones

--- ERPMVC/Controllers/InsurancesCertificateLineController.cs
+++ ERPMVC/Controllers/InsurancesCertificateLineController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
+using ERPMVC.DTO;
 using ERPMVC.Helpers;
 using ERPMVC.Models;
 using Kendo.Mvc.Extensions;
