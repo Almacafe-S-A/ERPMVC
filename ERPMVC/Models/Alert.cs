554b9e2fbd27133e1612a5cca7e3decb22b852c0
anjimenez <44504837+anjimenez@users.noreply.github.com>
2019-11-11 16:37:22 -0600

    VendorController, Model Alert, Views Alerts, ContactPerson, pvwAddVendor

--- ERPMVC/Models/Alert.cs
+++ ERPMVC/Models/Alert.cs
@@ -49,6 +49,7 @@ namespace ERPMVC.Models
         public string DescriptionAlert { get; set; }
         [Display(Name = "Tipo(s) de alerta")]
         public string Type { get; set; } ///OFAC , ONU ,PEPS si existe en uno o varios
+        public string AlertType { get; set; }
         public DateTime FechaCreacion { get; set; }
         public DateTime FechaModificacion { get; set; }
         public string UsuarioCreacion { get; set; }
