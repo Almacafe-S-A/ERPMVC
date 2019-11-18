3f3ed02ae07af8aa4079a78ba3572b1aa2372bf4
Brayan Interiano <44504858+binteriano0502@users.noreply.github.com>
2019-11-18 11:18:03 -0600

    CreditNote

--- ERPMVC/Controllers/CreditNoteController.cs
+++ ERPMVC/Controllers/CreditNoteController.cs
@@ -133,11 +133,13 @@ namespace ERPMVC.Controllers
                     _CreditNote.FechaCreacion = DateTime.Now;
                     _CreditNote.UsuarioCreacion = HttpContext.Session.GetString("user");
                     var insertresult = await Insert(_CreditNote);
-                    //if (_CreditNote.CAI==null || _CreditNote.CAI=="")
-                    //{
-                    //    string error = await result.Content.ReadAsStringAsync();
-                    //    return await Task.Run(() => BadRequest($" No existe un CAI activo para el punto de emisión."));
-                    //}
+                    var value = (insertresult.Result as ObjectResult).Value;
+
+                    CreditNote resultado = ((CreditNote)(value));
+                    if (resultado.CAI == null || resultado.CAI=="")
+                    {
+                        return await Task.Run(() => BadRequest("No existe un CAI activo para el punto de emisión"));
+                    }
 
                 }
                 else
