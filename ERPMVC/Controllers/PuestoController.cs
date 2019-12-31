b23fa89917095320d05145eeceb8aec34e2e842a
carlosdael29 <44511068+carlosdael29@users.noreply.github.com>
2019-12-31 10:00:06 -0600

    Changes-Views-Controller

--- ERPMVC/Controllers/PuestoController.cs
+++ ERPMVC/Controllers/PuestoController.cs
@@ -150,7 +150,7 @@ namespace ERPMVC.Controllers
 
                 if (_Puesto == null)
                 {
-                    _Puesto = new PuestoDTO { FechaCreacion = DateTime.Now};
+                    _Puesto = new PuestoDTO { FechaCreacion = DateTime.Now };
                 }
             }
             catch (Exception ex)
@@ -177,7 +177,7 @@ namespace ERPMVC.Controllers
                 string baseadress = config.Value.urlbase;
                 HttpClient _client = new HttpClient();
                 _client.DefaultRequestHeaders.Add("Authorization", "Bearer " + HttpContext.Session.GetString("token"));
-                var result1 = await _client.GetAsync(baseadress + "api/Puesto/GetPuestoByNombrePuesto/" + _Puesto.NombrePuesto+"/"+_Puesto.NombreDepartamento);
+                var result1 = await _client.GetAsync(baseadress + "api/Puesto/GetPuestoByNombrePuesto/" + _Puesto.NombrePuesto + "/" + _Puesto.NombreDepartamento);
                 string valorrespuesta1 = "";
 
                 if (result1.IsSuccessStatusCode)
@@ -293,7 +293,7 @@ namespace ERPMVC.Controllers
 
 
         [HttpPost]
-        public async Task<ActionResult<Puesto>> Delete(Int64 Id, Puesto _PuestoP)
+        public async Task<ActionResult<Puesto>> Delete(Int64 Id,[FromBody]Puesto _PuestoP)
         {
             Puesto _Puesto = _PuestoP;
             try
