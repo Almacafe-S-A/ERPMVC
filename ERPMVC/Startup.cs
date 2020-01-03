e452e2637ad4cf8cc05381dda1f1200153d08df3
Carlos Castillo <carlos.castillo@bi-dss.com>
2020-01-03 13:16:01 -0600

    CAmbios en el Idioma por defecto ne Startup, Validaciones en Autorizacionde Entregas

--- ERPMVC/Startup.cs
+++ ERPMVC/Startup.cs
@@ -160,13 +160,10 @@ namespace ERPMVC
 
             services.AddKendo();
             
-            
         
             services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
             services.AddSingleton<IAuthorizationPolicyProvider, AuthorizationPolicyProvider>();
             services.AddScoped<IAuthorizationHandler, HasScopeHandler>();
-            CultureInfo.DefaultThreadCurrentCulture = new CultureInfo("es-HN", false);
-            CultureInfo.DefaultThreadCurrentUICulture = new CultureInfo("es-HN", false);
         }
 
         private async Task<int> GetQuantityFailedRequest()
@@ -246,7 +243,29 @@ namespace ERPMVC
           
 
             app.UseAuthentication();
-          
+            var defaultDateCulture = "es-ES";
+            var ci = new CultureInfo(defaultDateCulture);
+            ci.NumberFormat.NumberDecimalSeparator = ".";
+            ci.NumberFormat.CurrencyDecimalSeparator = ".";
+            ci.NumberFormat.CurrencySymbol = "L";
+
+            // Configure the Localization middleware
+            app.UseRequestLocalization(new RequestLocalizationOptions
+            {
+                DefaultRequestCulture = new RequestCulture(ci),
+                SupportedCultures = new List<CultureInfo>
+    {
+        ci,
+    },
+                SupportedUICultures = new List<CultureInfo>
+    {
+        ci,
+    }
+            });
+
+            //CultureInfo.DefaultThreadCurrentCulture = new CultureInfo("es-HN", false);
+            //CultureInfo.DefaultThreadCurrentUICulture = new CultureInfo("es-HN", false);
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
