c75b315a8771e828532c3dae9d35702af1bef236
csolorzanof <carlos.solorzano@acklenavenue.com>
2020-03-10 10:38:18 -0600

    Cambio de maquina

---
+++ ERPMVC/Models/Bonificacion.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ERPMVC.Models
+{
+    public class Bonificacion
+    {
+        public long Id { get; set; }
+        public long EmpleadoId { get; set; }
+        public long TipoId { get; set; }
+        public double Monto { get; set; }
+        public DateTime FechaBono { get; set; }
+        public long EstadoId { get; set; }
+        public Estados Estado { get; set; }
+        public Employees Empleado { get; set; }
+        public DateTime FechaCreacion { get; set; }
+        public DateTime FechaModificacion { get; set; }
+        public string UsuarioModificacion { get; set; }
+        public string UsuarioCreacion { get; set; }
+    }
+}
