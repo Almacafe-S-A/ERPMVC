f9e8346670ed1fdc94d4d1aaffbb55d10cf123cb
Carlos Eduardo Castillo <carlosedcastillo@outlook.com>
2022-03-05 11:12:21 -0600

    Levantamiento de Inventario

--- ERPMVC/Controllers/Inventarios/InventarioFisicoController.cs
+++ ERPMVC/Controllers/Inventarios/InventarioFisicoController.cs
@@ -44,7 +44,7 @@ namespace ERPMVC.Controllers
             _principal = httpContextAccessor.HttpContext.User;
         }
 
-        [Authorize(Policy = "Inventarios.Solicitud Certificado Deposito")]
+        [Authorize(Policy = "Inventarios.InventarioFisico")]
         [HttpGet("[controller]/[action]")]
         public IActionResult Index()
         {
