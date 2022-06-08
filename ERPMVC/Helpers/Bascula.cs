902ca230f12772d997e4341225b5f556bba65f66
Carlos Eduardo Castillo <carlosedcastillo@outlook.com>
2022-06-08 16:28:13 -0600

    cambios en bascula

--- ERPMVC/Helpers/Bascula.cs
+++ ERPMVC/Helpers/Bascula.cs
@@ -23,41 +23,11 @@ namespace ERPMVC.Helpers
         Byte[] bytes = new Byte[256];
         //MainWindow mainwind = null;
         //public void Server(MainWindow wind)
-        public void Server()
+        
+        public async Task<string> ClienteTcpLectura(string ip, int puerto)
         {
-            this.tcpListener = new TcpListener(IPAddress.Any, port);
-            //this.listenThread = new Thread(new ThreadStart(ListenForClients));
-            this.listenThread.Start();
-
-        }
-        public async Task<string> ListenForClients()
-        {
-           // this.tcpListener = new TcpListener(localAddr., port);
-            //this.tcpListener.Start();
-            string peso = "";
-
-            while (peso.Length<1)
-            {
-                //blocks until a client has connected to the server
-                TcpClient client = this.tcpListener.AcceptTcpClient();
-
-                //create a thread to handle communication 
-                //with connected client
-                //Thread clientThread = new Thread(new ParameterizedThreadStart(HandleClientComm));
-                //clientThread.Start(client);
-                peso = await HandleClientComm(client);
-            }
-            //this.tcpListener.Stop();
-            return peso;
-        }
-        public async Task<string> HandleClientComm(object client)
-        {
-            //TcpClient tcpClient = (TcpClient)client;
-
-            TcpClient cliente = new TcpClient("192.168.0.7", 90);
+            TcpClient cliente = new TcpClient(ip, puerto);
             NetworkStream clientStream = cliente.GetStream();
-
-
 
             byte[] message = new byte[4096];
             int bytesRead;
@@ -69,13 +39,10 @@ namespace ERPMVC.Helpers
 
                 try
                 {
-                    //blocks until a client sends a message
                     bytesRead = clientStream.Read(message, 8, 14);
                 }
                 catch(Exception ex)
                 {
-                    //a socket error has occured
-                    // System.Windows.MessageBox.Show("socket");
                     Console.WriteLine("Error:" + ex.Message);
                     break;
                 }
@@ -84,13 +51,6 @@ namespace ERPMVC.Helpers
                     clientStream.Close();
                     cliente.Close();
                 }
-
-                //if (bytesRead == 0)
-                //{
-                //    //the client has disconnected from the server
-                //    // System.Windows.MessageBox.Show("disc");
-                //    break;
-                //}
 
                 //message has successfully been received
                 ASCIIEncoding encoder = new ASCIIEncoding();
@@ -102,8 +62,6 @@ namespace ERPMVC.Helpers
                 {
                     break;
                 }
-                //System.Windows.MessageBox.Show(encoder.GetString(message, 0, bytesRead));
-                // System.Diagnostics.Debug.WriteLine(encoder.GetString(message, 0, bytesRead));
             }
 
             cliente.Close();
