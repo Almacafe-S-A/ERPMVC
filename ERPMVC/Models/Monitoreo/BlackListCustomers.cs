6ec050961af9fabfbbe9712a43cada13563047a9
Carlos Eduardo Castillo <carlosedcastillo@outlook.com>
2022-08-24 12:25:30 -0600

    Se elimino del modelo de las listas mediaticas las referencias del cliente

--- ERPMVC/Models/Monitoreo/BlackListCustomers.cs
+++ ERPMVC/Models/Monitoreo/BlackListCustomers.cs
@@ -12,10 +12,7 @@ namespace ERPMVC.Models
         [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         [Display(Name = "Id")]
         public Int64 BlackListId { get; set; }
-        [Display(Name = "Cliente")]
-        public Int64? CustomerId { get; set; }
-        [Display(Name = "Cliente")]
-        public string CustomerName { get; set; }
+       
         public string Nombre { get; set; }
 
         [Display(Name = "Cliente referencia")]
