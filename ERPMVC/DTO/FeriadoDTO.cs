fee022b30831682aba5944ef6b71151260fd9f0d
Rafael Suazo <supahotfire16@hotmail.com>
2023-06-07 11:01:44 -0600

    67INC Modificacion Estructura Feriados

---
+++ ERPMVC/DTO/FeriadoDTO.cs
@@ -0,0 +1,17 @@
+﻿using ERPMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ERPMVC.DTO
+{
+    public class FeriadoDTO : Feriado
+    {
+        public List<Feriado> feriado { get; set; }
+
+        public int editar { get; set; } = 1;
+
+        public string token { get; set; }
+    }
+}
