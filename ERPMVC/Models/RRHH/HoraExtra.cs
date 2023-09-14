0a9c4b00c7365efdd364180d1570bca74c7a44a1
Rafael Suazo <supahotfire16@hotmail.com>
2023-09-14 08:04:52 -0600

    36INC Horas Extras Empleados

--- ERPMVC/Models/RRHH/HoraExtra.cs
+++ ERPMVC/Models/RRHH/HoraExtra.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,21 +9,31 @@ namespace ERPMVC.Models
 {
     public class HoraExtra
     {
+        [Display(Name = "Id")]
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public long Id { get; set; }
 
+        [Required]
         public long IdBiometrico { get; set; }
 
+        [Required]
         public long IdEmpleado { get; set; }
 
+        [Required]
         public int Horas { get; set; }
+
+        [Required]
         public int Minutos { get; set; }
 
         public Biometrico Encabezado { get; set; }
 
+        [ForeignKey("IdEmpleado")]
         public Employees Empleado { get; set; }
 
+        [Required]
         public long IdEstado { get; set; }
 
+        [ForeignKey("IdEstado")]
         public Estados Estado { get; set; }
     }
 }
